428fddd07dce5e318211edf9136df231eca89ee9
ashen <40572061+saya-ashen@users.noreply.github.com>
2024-12-16 10:37:33 +0800

    Update build_pve_qemu.sh
    git submodule set-url

--- build_pve_qemu.sh
+++ build_pve_qemu.sh
@@ -7,10 +7,7 @@ df -h
 git clone git://git.proxmox.com/git/pve-qemu.git
 cd pve-qemu
 git reset --hard c4efa30b307fc15df5c00f353494d1aec1702680
-git submodule update --init --recursive 2>/dev/null || true
-sed -i 's|https://github.com/Zeex/subhook.git|https://github.com/tianocore/edk2-subhook.git|g' qemu/roms/edk2/.gitmodules
-echo "再次更新子模块"
-git submodule update --init --recursive
+git submodule set-url qemu/roms/edk2 https://github.com/tianocore/edk2-subhook.git
 apt install devscripts -y
 mk-build-deps --install
 make
