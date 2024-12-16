897b819862dca7dafe81f1d05d0d4b9624f0e01a
ashen <40572061+saya-ashen@users.noreply.github.com>
2024-12-16 10:23:36 +0800

    Update build_pve_qemu.sh
    fix 2

--- build_pve_qemu.sh
+++ build_pve_qemu.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -e  # 启用错误自动退出
 sudo apt-get update
 sudo apt-get install libacl1-dev libaio-dev libattr1-dev libcap-ng-dev libcurl4-gnutls-dev libepoxy-dev libfdt-dev libgbm-dev libglusterfs-dev libgnutls28-dev libiscsi-dev libjpeg-dev libnuma-dev libpci-dev libpixman-1-dev libproxmox-backup-qemu0-dev librbd-dev libsdl1.2-dev libseccomp-dev libslirp-dev libspice-protocol-dev libspice-server-dev libsystemd-dev liburing-dev libusb-1.0-0-dev libusbredirparser-dev libvirglrenderer-dev meson python3-sphinx python3-sphinx-rtd-theme quilt xfslibs-dev
 ls
@@ -7,6 +9,8 @@ cd pve-qemu
 git reset --hard c4efa30b307fc15df5c00f353494d1aec1702680
 git submodule update --init --recursive
 sed -i 's|https://github.com/Zeex/subhook.git|https://github.com/tianocore/edk2-subhook.git|g' qemu/roms/edk2/.gitmodules
+echo "再次更新子模块"
+git submodule update --init --recursive
 apt install devscripts -y
 mk-build-deps --install
 make
